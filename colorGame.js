b1f75ffef86cee4dddbe05f7aee9ae7536f89d07
shadwan <shadman.skb@gmail.com>
2016-11-16 19:32:06 +0600

    Color Game

---
+++ colorGame.js
@@ -0,0 +1,46 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    squares[i].style.background = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        //grab color of clicked square
+
+        var clickedColor = this.style.background;
+
+        //compare color to pickedcolor
+
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
+        } else {
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+
+    });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for (var i = 0; i<squares.length; i++){
+        //change each color to macth given color 
+        squares[i].style.background = color;
+    }
+}
